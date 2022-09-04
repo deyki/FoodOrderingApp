b963024beda0436617142df5647f90e53d565f5d
deyki <deikioveca@gmail.com>
2022-09-04 12:42:51 +0300

    A little change

--- src/main/java/deyki/FoodOrdering/service/UserService.java
+++ src/main/java/deyki/FoodOrdering/service/UserService.java
@@ -2,6 +2,7 @@ package deyki.FoodOrdering.service;
 
 import deyki.FoodOrdering.domain.bindingModel.user.UserBindingModel;
 import deyki.FoodOrdering.domain.responseModel.user.LoginResponseModel;
+import deyki.FoodOrdering.error.UserNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -9,5 +10,5 @@ public interface UserService extends UserDetailsService {
 
     void register(UserBindingModel userBindingModel);
 
-    LoginResponseModel login(UserBindingModel userBindingModel);
+    LoginResponseModel login(UserBindingModel userBindingModel) throws UserNotFoundException;
 }
