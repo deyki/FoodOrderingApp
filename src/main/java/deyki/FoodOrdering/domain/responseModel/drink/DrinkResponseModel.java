de291de79d53b1458f1f0d0ada252936311c62b5
deyki <deikioveca@gmail.com>
2022-09-05 21:25:14 +0300

    Response model for Drink created

---
+++ src/main/java/deyki/FoodOrdering/domain/responseModel/drink/DrinkResponseModel.java
@@ -0,0 +1,13 @@
+package deyki.FoodOrdering.domain.responseModel.drink;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DrinkResponseModel {
+
+    private String drink;
+}
