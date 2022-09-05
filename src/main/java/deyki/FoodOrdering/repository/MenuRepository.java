3c6cbb18f39c848da45fe99f85b9c9f85f582c83
deyki <deikioveca@gmail.com>
2022-09-05 21:22:28 +0300

    Menu repo created

---
+++ src/main/java/deyki/FoodOrdering/repository/MenuRepository.java
@@ -0,0 +1,11 @@
+package deyki.FoodOrdering.repository;
+
+import deyki.FoodOrdering.domain.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
