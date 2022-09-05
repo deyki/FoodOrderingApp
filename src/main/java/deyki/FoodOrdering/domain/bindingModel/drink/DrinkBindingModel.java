bb3f4e582fcae6703f01aec62cd57249b3526a9b
deyki <deikioveca@gmail.com>
2022-09-05 21:24:45 +0300

    DrinkBindingModel added

---
+++ src/main/java/deyki/FoodOrdering/domain/bindingModel/drink/DrinkBindingModel.java
@@ -0,0 +1,13 @@
+package deyki.FoodOrdering.domain.bindingModel.drink;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DrinkBindingModel {
+
+    private String drink;
+}
