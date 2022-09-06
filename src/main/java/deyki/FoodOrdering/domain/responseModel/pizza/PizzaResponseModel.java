1c9233ea915071f68748dd21d88cfaf5196c6ac1
deyki <deikioveca@gmail.com>
2022-09-06 13:10:06 +0300

    PizzaResponseModel

---
+++ src/main/java/deyki/FoodOrdering/domain/responseModel/pizza/PizzaResponseModel.java
@@ -0,0 +1,13 @@
+package deyki.FoodOrdering.domain.responseModel.pizza;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PizzaResponseModel {
+
+    private String pizza;
+}
