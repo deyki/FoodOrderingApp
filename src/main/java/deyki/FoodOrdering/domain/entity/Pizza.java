9e95837a1923f1331ff89302626b43fc9932a3d2
deyki <deikioveca@gmail.com>
2022-09-06 13:09:46 +0300

    Pizza entity

---
+++ src/main/java/deyki/FoodOrdering/domain/entity/Pizza.java
@@ -0,0 +1,23 @@
+package deyki.FoodOrdering.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "pizzas")
+public class Pizza {
+
+    @Id
+    @SequenceGenerator(name = "pizza_sequence", sequenceName = "pizza_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pizza_sequence")
+    private Long pizzaId;
+
+    @Column(name = "pizza")
+    private String pizza;
+}
