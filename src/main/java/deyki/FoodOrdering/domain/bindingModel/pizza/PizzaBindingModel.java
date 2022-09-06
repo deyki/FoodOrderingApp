e13ed1e288617b65677a86c24679efc6c0f77200
deyki <deikioveca@gmail.com>
2022-09-06 13:09:05 +0300

    PizzaBindingModel

---
+++ src/main/java/deyki/FoodOrdering/domain/bindingModel/pizza/PizzaBindingModel.java
@@ -0,0 +1,13 @@
+package deyki.FoodOrdering.domain.bindingModel.pizza;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PizzaBindingModel {
+
+    private String pizza;
+}
