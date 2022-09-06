7fe9e9dd1b20d11422b4891943e478224da12fa9
deyki <deikioveca@gmail.com>
2022-09-06 13:10:18 +0300

    PizzaService

---
+++ src/main/java/deyki/FoodOrdering/service/PizzaService.java
@@ -0,0 +1,18 @@
+package deyki.FoodOrdering.service;
+
+import deyki.FoodOrdering.domain.bindingModel.pizza.PizzaBindingModel;
+import deyki.FoodOrdering.domain.responseModel.pizza.PizzaResponseModel;
+import deyki.FoodOrdering.error.PizzaNotFoundException;
+
+public interface PizzaService {
+
+    void createPizza(PizzaBindingModel pizzaBindingModel);
+
+    PizzaResponseModel getPizzaById(Long pizzaId) throws PizzaNotFoundException;
+
+    PizzaResponseModel getPizzaByName(PizzaBindingModel pizzaBindingModel) throws PizzaNotFoundException;
+
+    void deletePizzaById(Long pizzaId) throws PizzaNotFoundException;
+
+    void deletePizzaByName(PizzaBindingModel pizzaBindingModel) throws PizzaNotFoundException;
+}
---
+++ src/main/java/deyki/FoodOrdering/service/impl/PizzaServiceImpl.java
@@ -0,0 +1,71 @@
+package deyki.FoodOrdering.service.impl;
+
+import deyki.FoodOrdering.domain.bindingModel.pizza.PizzaBindingModel;
+import deyki.FoodOrdering.domain.entity.Pizza;
+import deyki.FoodOrdering.domain.responseModel.pizza.PizzaResponseModel;
+import deyki.FoodOrdering.error.PizzaNotFoundException;
+import deyki.FoodOrdering.repository.PizzaRepository;
+import deyki.FoodOrdering.service.PizzaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PizzaServiceImpl implements PizzaService {
+
+    private final PizzaRepository pizzaRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public PizzaServiceImpl(PizzaRepository pizzaRepository, ModelMapper modelMapper) {
+        this.pizzaRepository = pizzaRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    @Override
+    public void createPizza(PizzaBindingModel pizzaBindingModel) {
+
+        Pizza pizza = modelMapper.map(pizzaBindingModel, Pizza.class);
+
+        pizzaRepository.save(pizza);
+    }
+
+    @Override
+    public PizzaResponseModel getPizzaById(Long pizzaId) throws PizzaNotFoundException {
+
+        return pizzaRepository
+                .findById(pizzaId)
+                .map(pizza -> modelMapper.map(pizza, PizzaResponseModel.class))
+                .orElseThrow(() -> new PizzaNotFoundException(String.format("Pizza with id: %d not found!", pizzaId)));
+    }
+
+    @Override
+    public PizzaResponseModel getPizzaByName(PizzaBindingModel pizzaBindingModel) throws PizzaNotFoundException {
+
+        return pizzaRepository
+                .findByName(pizzaBindingModel.getPizza())
+                .map(pizza -> modelMapper.map(pizza, PizzaResponseModel.class))
+                .orElseThrow(() -> new PizzaNotFoundException(String.format("Pizza %s not found!", pizzaBindingModel.getPizza())));
+    }
+
+    @Override
+    public void deletePizzaById(Long pizzaId) throws PizzaNotFoundException {
+
+        Pizza pizza = pizzaRepository
+                .findById(pizzaId)
+                .orElseThrow(() -> new PizzaNotFoundException(String.format("Pizza with id: %d not found!", pizzaId)));
+
+        pizzaRepository.deleteById(pizza.getPizzaId());
+    }
+
+    @Override
+    public void deletePizzaByName(PizzaBindingModel pizzaBindingModel) throws PizzaNotFoundException {
+
+        Pizza pizza = pizzaRepository
+                .findByName(pizzaBindingModel.getPizza())
+                .orElseThrow(() -> new PizzaNotFoundException(String.format("Pizza %s not found!", pizzaBindingModel.getPizza())));
+
+        pizzaRepository.delete(pizza);
+    }
+}
