80e4b9ff2d3d3c16c1d608f49d559772af48e61b
deyki <deikioveca@gmail.com>
2022-09-06 13:09:29 +0300

    PizzaNotFoundException

---
+++ src/main/java/deyki/FoodOrdering/error/PizzaNotFoundException.java
@@ -0,0 +1,23 @@
+package deyki.FoodOrdering.error;
+
+public class PizzaNotFoundException extends Exception{
+    public PizzaNotFoundException() {
+        super();
+    }
+
+    public PizzaNotFoundException(String message) {
+        super(message);
+    }
+
+    public PizzaNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PizzaNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected PizzaNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
