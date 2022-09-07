ebe78d2ffc5aac3a1aa54e32d1af3c7af0078392
deyki <deikioveca@gmail.com>
2022-09-07 15:42:06 +0300

    DessertBindingModel

---
+++ src/main/java/deyki/FoodOrdering/domain/bindingModel/dessert/DessertBindingModel.java
@@ -0,0 +1,13 @@
+package deyki.FoodOrdering.domain.bindingModel.dessert;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DessertBindingModel {
+
+    private String dessert;
+}
