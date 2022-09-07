ae8c9fe9ae88fa7dc47c2c0537fee8372e0b3312
deyki <deikioveca@gmail.com>
2022-09-07 14:55:54 +0300

    MainDishResponseModel

---
+++ src/main/java/deyki/FoodOrdering/domain/responseModel/mainDish/MainDishResponseModel.java
@@ -0,0 +1,13 @@
+package deyki.FoodOrdering.domain.responseModel.mainDish;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MainDishResponseModel {
+
+    private String mainDish;
+}
