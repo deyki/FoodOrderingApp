69d4acd841c286b7f91607567fa11bd4deb9d648
deyki <deikioveca@gmail.com>
2022-09-07 15:42:16 +0300

    Dessert

---
+++ src/main/java/deyki/FoodOrdering/domain/entity/Dessert.java
@@ -0,0 +1,23 @@
+package deyki.FoodOrdering.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "desserts")
+public class Dessert {
+
+    @Id
+    @SequenceGenerator(name = "dessert_sequence", sequenceName = "dessert_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dessert_sequence")
+    private Long dessertId;
+
+    @Column(name = "dessert")
+    private String dessert;
+}
