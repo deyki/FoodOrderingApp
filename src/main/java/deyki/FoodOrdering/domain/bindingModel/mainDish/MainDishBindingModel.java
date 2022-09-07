0840c4b521e2d68fd5388ba39f67c36eec4288f1
deyki <deikioveca@gmail.com>
2022-09-07 15:02:53 +0300

    MainDishBindingModel

---
+++ src/main/java/deyki/FoodOrdering/domain/bindingModel/mainDish/MainDishBindingModel.java
@@ -0,0 +1,13 @@
+package deyki.FoodOrdering.domain.bindingModel.mainDish;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MainDishBindingModel {
+
+    private String mainDish;
+}
