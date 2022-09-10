bcdb5f86223821cc363b2d786439a4071b07203d
deyki <deikioveca@gmail.com>
2022-09-10 07:54:37 +0300

    MenuServiceImpl

--- src/main/java/deyki/FoodOrdering/service/impl/MenuServiceImpl.java
+++ src/main/java/deyki/FoodOrdering/service/impl/MenuServiceImpl.java
@@ -1,22 +1,48 @@
 package deyki.FoodOrdering.service.impl;
 
-import deyki.FoodOrdering.repository.DrinkRepository;
+import deyki.FoodOrdering.domain.entity.Menu;
+import deyki.FoodOrdering.domain.responseModel.menu.MenuResponseModel;
+import deyki.FoodOrdering.error.MenuAlreadyInitializedException;
+import deyki.FoodOrdering.error.MenuNotInitializedException;
 import deyki.FoodOrdering.repository.MenuRepository;
 import deyki.FoodOrdering.service.MenuService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class MenuServiceImpl implements MenuService {
 
     private final MenuRepository menuRepository;
-    private final DrinkRepository drinkRepository;
+    private final ModelMapper modelMapper;
 
-    @Autowired
-    public MenuServiceImpl(MenuRepository menuRepository, DrinkRepository drinkRepository) {
+    public MenuServiceImpl(MenuRepository menuRepository, ModelMapper modelMapper) {
         this.menuRepository = menuRepository;
-        this.drinkRepository = drinkRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public void initMenu() throws MenuAlreadyInitializedException {
+
+        if (menuRepository.count() == 0) {
+
+            Menu mainMenu = new Menu();
+            mainMenu.setName("Main menu");
+
+            menuRepository.save(mainMenu);
+        } else {
+
+            throw new MenuAlreadyInitializedException("Menu is already initialized!");
+        }
+    }
+
+    @Override
+    public MenuResponseModel getMenu() throws MenuNotInitializedException {
+
+        Menu menu = menuRepository
+                .findByName("Main menu")
+                .orElseThrow(() -> new MenuNotInitializedException("Main menu is not initialized!"));
+
+        return modelMapper.map(menu, MenuResponseModel.class);
     }
 }
 
