2a3a2b89364632cb039fed334027e7b57fc07aae
deyki <deikioveca@gmail.com>
2022-09-10 07:54:59 +0300

    MenuController

--- src/main/java/deyki/FoodOrdering/controller/MenuController.java
+++ src/main/java/deyki/FoodOrdering/controller/MenuController.java
@@ -1,7 +1,12 @@
 package deyki.FoodOrdering.controller;
 
+import deyki.FoodOrdering.domain.responseModel.menu.MenuResponseModel;
+import deyki.FoodOrdering.error.MenuAlreadyInitializedException;
+import deyki.FoodOrdering.error.MenuNotInitializedException;
 import deyki.FoodOrdering.service.impl.MenuServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -14,5 +19,19 @@ public class MenuController {
     @Autowired
     public MenuController(MenuServiceImpl menuService) {
         this.menuService = menuService;
+    }
+
+    @PostMapping("/init")
+    public ResponseEntity<String> initMenu() throws MenuAlreadyInitializedException {
+
+        menuService.initMenu();
+
+        return ResponseEntity.status(HttpStatus.OK).body("Menu initialized!");
+    }
+
+    @GetMapping("/getMenu")
+    public ResponseEntity<MenuResponseModel> getMenu() throws MenuNotInitializedException {
+
+        return ResponseEntity.status(HttpStatus.OK).body(menuService.getMenu());
     }
 }
