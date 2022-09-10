f684134954c19009972001ec4590345832065901
deyki <deikioveca@gmail.com>
2022-09-10 07:54:27 +0300

    MenuService

--- src/main/java/deyki/FoodOrdering/service/MenuService.java
+++ src/main/java/deyki/FoodOrdering/service/MenuService.java
@@ -1,5 +1,12 @@
 package deyki.FoodOrdering.service;
 
+import deyki.FoodOrdering.domain.responseModel.menu.MenuResponseModel;
+import deyki.FoodOrdering.error.MenuAlreadyInitializedException;
+import deyki.FoodOrdering.error.MenuNotInitializedException;
+
 public interface MenuService {
 
+    void initMenu() throws MenuAlreadyInitializedException;
+
+    MenuResponseModel getMenu() throws MenuNotInitializedException;
 }
