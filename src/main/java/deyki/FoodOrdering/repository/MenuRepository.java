c1541a14aaf538433146284b4d613149091455b6
deyki <deikioveca@gmail.com>
2022-09-10 07:54:46 +0300

    MenuRepository

--- src/main/java/deyki/FoodOrdering/repository/MenuRepository.java
+++ src/main/java/deyki/FoodOrdering/repository/MenuRepository.java
@@ -2,10 +2,15 @@ package deyki.FoodOrdering.repository;
 
 import deyki.FoodOrdering.domain.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    @Query("SELECT m FROM Menu m WHERE m.name = ?1")
+    Optional<Menu> findByName(@Param("name") String name);
 }
