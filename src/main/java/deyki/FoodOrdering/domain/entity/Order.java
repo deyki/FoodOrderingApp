005e1a03203d9bc1491a877fe90db26a1c081944
deyki <deikioveca@gmail.com>
2022-09-11 12:16:00 +0300

    Order

---
+++ src/main/java/deyki/FoodOrdering/domain/entity/Order.java
@@ -0,0 +1,50 @@
+package deyki.FoodOrdering.domain.entity;
+
+import deyki.FoodOrdering.domain.enums.OrderType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @SequenceGenerator(name = "order_sequence", sequenceName = "order_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_sequence")
+    private Long orderId;
+
+    @Column(name = "order_type", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private OrderType orderType;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "userId")
+    private User user;
+
+    @Column(name = "delivery_address")
+    private String deliveryAddress;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    @Column(name = "name")
+    private String drink;
+
+    @Column(name = "pizza")
+    private String pizza;
+
+    @Column(name = "main_dish")
+    private String mainDish;
+
+    @Column(name = "salad")
+    private String salad;
+
+    @Column(name = "dessert")
+    private String dessert;
+}
