8e8e434e25a250f87fa9990294b7c29abffa4d2c
deyki <deikioveca@gmail.com>
2022-09-11 12:16:19 +0300

    OrderRepository

---
+++ src/main/java/deyki/FoodOrdering/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package deyki.FoodOrdering.repository;
+
+import deyki.FoodOrdering.domain.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
