71900801dbdee864cf08e5df51bccac8aa420236
deyki <deikioveca@gmail.com>
2022-09-11 12:17:38 +0300

    OrderType

---
+++ src/main/java/deyki/FoodOrdering/domain/enums/OrderType.java
@@ -0,0 +1,11 @@
+package deyki.FoodOrdering.domain.enums;
+
+public enum OrderType {
+    FullOrder,
+    MixedOrder,
+    DrinkOrder,
+    PizzaOrder,
+    MainDishOrder,
+    SaladOrder,
+    DessertOrder
+}
