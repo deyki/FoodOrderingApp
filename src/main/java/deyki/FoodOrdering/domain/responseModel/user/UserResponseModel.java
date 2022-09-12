d01dc203bcc8a226cd704171bd93e797253a9f61
deyki <deikioveca@gmail.com>
2022-09-12 11:04:44 +0300

    UserResponseModel

---
+++ src/main/java/deyki/FoodOrdering/domain/responseModel/user/UserResponseModel.java
@@ -0,0 +1,13 @@
+package deyki.FoodOrdering.domain.responseModel.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseModel {
+
+    private String username;
+}
