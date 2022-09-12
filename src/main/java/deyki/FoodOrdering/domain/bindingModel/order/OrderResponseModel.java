6a3e7fff4c70ec9f05231fe09529b158c44f6e23
deyki <deikioveca@gmail.com>
2022-09-12 11:11:30 +0300

    OrderResponseModel

---
+++ src/main/java/deyki/FoodOrdering/domain/bindingModel/order/OrderResponseModel.java
@@ -0,0 +1,19 @@
+package deyki.FoodOrdering.domain.bindingModel.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderResponseModel {
+
+    private String orderType;
+    private String deliveryAddress;
+    private String drink;
+    private String pizza;
+    private String mainDish;
+    private String salad;
+    private String dessert;
+}
