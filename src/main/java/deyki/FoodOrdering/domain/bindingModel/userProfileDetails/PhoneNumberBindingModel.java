677fef53d3f9f3f8b436dbceaf74803a92222cd6
deyki <deikioveca@gmail.com>
2022-09-13 12:19:20 +0300

    Binding model

---
+++ src/main/java/deyki/FoodOrdering/domain/bindingModel/userProfileDetails/PhoneNumberBindingModel.java
@@ -0,0 +1,13 @@
+package deyki.FoodOrdering.domain.bindingModel.userProfileDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhoneNumberBindingModel {
+
+    private Integer phoneNumber;
+}
