64ef6f289891c3b43d21e16a663d8b2bb56dee04
deyki <deikioveca@gmail.com>
2022-09-13 12:21:40 +0300

    UserProfileDetailsRepository

---
+++ src/main/java/deyki/FoodOrdering/repository/UserProfileDetailsRepository.java
@@ -0,0 +1,19 @@
+package deyki.FoodOrdering.repository;
+
+import deyki.FoodOrdering.domain.entity.UserProfileDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserProfileDetailsRepository extends JpaRepository<UserProfileDetails, Long> {
+
+    @Query("SELECT u FROM UserProfileDetails u WHERE u.email = ?1")
+    Optional<UserProfileDetails> findByEmail(@Param("email") String email);
+
+    @Query("SELECT u FROM UserProfileDetails u WHERE u.phoneNumber = ?1")
+    Optional<UserProfileDetails> findByPhoneNumber(@Param("phoneNumber") Integer phoneNumber);
+}
