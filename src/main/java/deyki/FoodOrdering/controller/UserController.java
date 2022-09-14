c71b6dce370c053d0ea358bfee66d13916ac8240
deyki <deikioveca@gmail.com>
2022-09-14 20:10:49 +0300

    updateUsernameById and updatePasswordById handlers added

--- src/main/java/deyki/FoodOrdering/controller/UserController.java
+++ src/main/java/deyki/FoodOrdering/controller/UserController.java
@@ -1,15 +1,18 @@
 package deyki.FoodOrdering.controller;
 
+import deyki.FoodOrdering.domain.bindingModel.user.NewPasswordBindingModel;
+import deyki.FoodOrdering.domain.bindingModel.user.NewUsernameBindingModel;
 import deyki.FoodOrdering.domain.bindingModel.userProfileDetails.EmailBindingModel;
 import deyki.FoodOrdering.domain.bindingModel.userProfileDetails.PhoneNumberBindingModel;
 import deyki.FoodOrdering.domain.bindingModel.userProfileDetails.UpdateDetailsBindingModel;
 import deyki.FoodOrdering.domain.bindingModel.userProfileDetails.UserProfileDetailsBindingModel;
 import deyki.FoodOrdering.domain.responseModel.userProfileDetails.UserProfileDetailsResponseModel;
+import deyki.FoodOrdering.error.IncorrectPasswordException;
 import deyki.FoodOrdering.error.UserNotFoundException;
 import deyki.FoodOrdering.error.UserProfileDetailsNotFoundException;
 import deyki.FoodOrdering.service.impl.UserProfileDetailsServiceImpl;
+import deyki.FoodOrdering.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.config.RepositoryConfigurationSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,10 +22,12 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserProfileDetailsServiceImpl userProfileDetailsService;
+    private final UserServiceImpl userService;
 
     @Autowired
-    public UserController(UserProfileDetailsServiceImpl userProfileDetailsService) {
+    public UserController(UserProfileDetailsServiceImpl userProfileDetailsService, UserServiceImpl userService) {
         this.userProfileDetailsService = userProfileDetailsService;
+        this.userService = userService;
     }
 
     @PostMapping("/create")
@@ -61,5 +66,17 @@ public class UserController {
     public ResponseEntity<String> getPointsByUserId(@PathVariable Long userId) throws UserProfileDetailsNotFoundException {
 
         return ResponseEntity.status(HttpStatus.OK).body(userProfileDetailsService.getPointsById(userId));
+    }
+
+    @PutMapping("/updateUsernameById/{userId}")
+    public ResponseEntity<String> updateUsernameById(@PathVariable Long userId, @RequestBody NewUsernameBindingModel newUsernameBindingModel) throws UserNotFoundException {
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUsernameById(userId, newUsernameBindingModel));
+    }
+
+    @PutMapping("/updatePasswordById/{userId}")
+    public ResponseEntity<String> updatePasswordById(@PathVariable Long userId, @RequestBody NewPasswordBindingModel newPasswordBindingModel) throws UserNotFoundException, IncorrectPasswordException {
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updatePasswordById(userId, newPasswordBindingModel));
     }
 }
