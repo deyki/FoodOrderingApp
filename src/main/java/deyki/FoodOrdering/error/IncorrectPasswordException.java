7d96606bca2308ae8ca1b989c14ae9bac2a92343
deyki <deikioveca@gmail.com>
2022-09-14 20:07:37 +0300

    IncorrectPasswordException

---
+++ src/main/java/deyki/FoodOrdering/error/IncorrectPasswordException.java
@@ -0,0 +1,23 @@
+package deyki.FoodOrdering.error;
+
+public class IncorrectPasswordException extends Exception{
+    public IncorrectPasswordException() {
+        super();
+    }
+
+    public IncorrectPasswordException(String message) {
+        super(message);
+    }
+
+    public IncorrectPasswordException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IncorrectPasswordException(Throwable cause) {
+        super(cause);
+    }
+
+    protected IncorrectPasswordException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
