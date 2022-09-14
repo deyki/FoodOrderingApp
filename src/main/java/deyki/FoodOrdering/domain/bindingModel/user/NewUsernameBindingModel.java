251a2feee21f54b1cb4ab334eecef8f80cedb01f
deyki <deikioveca@gmail.com>
2022-09-14 20:08:54 +0300

    NewUsernameBindingModel

---
+++ src/main/java/deyki/FoodOrdering/domain/bindingModel/user/NewUsernameBindingModel.java
@@ -0,0 +1,13 @@
+package deyki.FoodOrdering.domain.bindingModel.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewUsernameBindingModel {
+
+    private String username;
+}
