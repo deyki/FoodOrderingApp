52a41ab065ca6da90686d9296c8f2cfa564a978d
deyki <deikioveca@gmail.com>
2022-09-15 09:52:00 +0300

    DeliveryAddressResponseModel

---
+++ src/main/java/deyki/FoodOrdering/domain/responseModel/order/DeliveryAddressResponseModel.java
@@ -0,0 +1,13 @@
+package deyki.FoodOrdering.domain.responseModel.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryAddressResponseModel {
+
+    private String deliveryAddress;
+}
